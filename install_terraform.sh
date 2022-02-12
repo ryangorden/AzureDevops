a4bcd5ea980092d087031da44c8f101031d1a715
ryangorden <63272391+ryangorden@users.noreply.github.com>
2022-02-12 17:58:56 -0600

    Update install_terraform.sh

--- install_terraform.sh
+++ install_terraform.sh
@@ -3,4 +3,3 @@ sudo apt install -y unzip
 wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
 unzip terraform_"$tf_version"_linux_amd64.zip"
 sudo mv terraform /usr/local/bin/
-rm terraform_"$tf_version"_linux_amd64.zip
